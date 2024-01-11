e9a59f97a61703b57bbf8fbded3323044a6e9f96
Calum <calum.mj.russell@gmail.com>
2024-01-11 17:54:03 +0000

    basic keystore working

--- src/main/java/com/selene/KeyStore.java
+++ src/main/java/com/selene/KeyStore.java
@@ -6,55 +6,90 @@ import java.lang.foreign.ValueLayout;
 import java.nio.charset.StandardCharsets;
 
 public class KeyStore {
+    /*
+        One of the potential problems here is that we are writing an INT and BYTE which are potentially misaligned
+        if we are writing to the same MemorySegment when writing BYTE of length that aren't % 4. We can use
+        UNALIGNED ValueLayout but the docs seem to say this isn't a good idea and might not be portable.
+
+        So the solution is to hold two MemorySegments, one that holds the INT length and this is used to index into the
+        BYTE MemorySegment that is holding the actual key strings.
+     */
 
     private static final long INT_SIZE = 4;
-    private static final long INITIAL_SIZE = 100;
-    private long currentSize;
+    private long currentValueSize;
+    private long currentLengthSize;
     private Arena arena;
-    private MemorySegment keySegment;
-    private long writePosition;
+    private MemorySegment lengthSegment;
+    private MemorySegment valueSegment;
+    private long valueWritePosition;
+    private long lengthWritePosition;
 
-    private void resize() {
-        Arena newArena = Arena.ofConfined();
-        MemorySegment newSegment = newArena.allocate(this.currentSize * 2, 1);
-
-        //MemorySegment.copy(this.keySegment, 0, newSegment, 0, this.currentSize);
-        newSegment.copyFrom(this.keySegment);
-        this.arena = newArena;
-        this.keySegment = newSegment;
-        this.currentSize = this.currentSize * 2;
+    private void resizeLengthSegment() {
+        MemorySegment newSegment = this.arena.allocate(this.currentLengthSize * 2, 1);
+        newSegment.copyFrom(this.lengthSegment);
+        this.lengthSegment = newSegment;
+        this.currentLengthSize = this.currentLengthSize * 2;
     }
 
-    //Only supports indexed queries
+    private void resizeValueSegment() {
+        MemorySegment newSegment = this.arena.allocate(this.currentValueSize * 2, 1);
+        newSegment.copyFrom(this.valueSegment);
+        this.valueSegment = newSegment;
+        this.currentValueSize = this.currentValueSize * 2;
+    }
+
     public byte[] get(long offset) {
-        long length = this.keySegment.get(ValueLayout.JAVA_INT, offset);
+        // offset is the real starting position within the memory segment, not an index into array-like
+        long length = this.lengthSegment.get(ValueLayout.JAVA_INT, offset);
+        long startingPositon = this.lengthSegment.get(ValueLayout.JAVA_INT, offset+ 4);
+
         byte[] result = new byte[(int) length];
-        for (int i = 0; i < length; i++) {
-            result[i] = this.keySegment.get(ValueLayout.JAVA_BYTE, offset + 4 + i);
+        for (int i=0; i < length; i++) {
+            result[i] = this.valueSegment.get(ValueLayout.JAVA_BYTE, startingPositon + i);
         }
         return result;
     }
 
+    private boolean checkForValueResize(int newStringLength) {
+        return newStringLength + 4 > this.currentValueSize - this.valueWritePosition;
+    }
+
+    private boolean checkForLengthResize() {
+        return this.lengthWritePosition < this.currentLengthSize + 2;
+    }
+
     public long add(String key) {
         int len = key.length();
-        if (len + INT_SIZE > this.currentSize - this.writePosition) {
-            this.resize();
+        if (checkForValueResize(len)) {
+            this.resizeValueSegment();
         }
 
-        long startPosition = this.writePosition;
-        this.keySegment.set(ValueLayout.JAVA_INT, this.writePosition, len);
-        this.writePosition += INT_SIZE;
-        for (byte b: key.getBytes(StandardCharsets.UTF_8)) {
-            this.keySegment.set(ValueLayout.JAVA_BYTE, this.writePosition, b);
-            this.writePosition++;
+        if (checkForLengthResize()) {
+            this.resizeLengthSegment();
         }
-        return startPosition;
+
+        // Write length
+        this.lengthSegment.set(ValueLayout.JAVA_INT, this.lengthWritePosition, len);
+        // Write starting position of value in valueSegment
+        this.lengthWritePosition+=4;
+        this.lengthSegment.set(ValueLayout.JAVA_INT, this.lengthWritePosition, (int) this.valueWritePosition);
+        this.lengthWritePosition+=4;
+
+        for (byte b: key.getBytes(StandardCharsets.UTF_8)) {
+            this.valueSegment.set(ValueLayout.JAVA_BYTE, this.valueWritePosition, b);
+            this.valueWritePosition++;
+        }
+        // We return the index into the lengthSegment, which can then retrieve from the valueSegment
+        return this.lengthWritePosition-8;
     }
 
     public KeyStore() {
-        this.arena = Arena.ofConfined();
-        this.currentSize = INITIAL_SIZE;
-        this.keySegment = this.arena.allocate(this.currentSize, 1);
-        this.writePosition = 0;
+        this.arena = Arena.ofAuto();
+        this.currentLengthSize = 100;
+        this.currentValueSize = 100;
+        this.lengthSegment = this.arena.allocate(this.currentLengthSize, 4);
+        this.valueSegment = this.arena.allocate(this.currentValueSize, 1);
+        this.valueWritePosition = 0;
+        this.lengthWritePosition = 0;
     }
 }
--- src/test/java/com/selene/KeyStoreTest.java
+++ src/test/java/com/selene/KeyStoreTest.java
@@ -2,6 +2,10 @@ package com.selene;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.foreign.Arena;
+import java.lang.foreign.ValueLayout;
+import java.nio.charset.StandardCharsets;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class KeyStoreTest {
@@ -10,16 +14,16 @@ class KeyStoreTest {
     void testInsertingRetrieving() {
         KeyStore store = new KeyStore();
         long idx0 = store.add("test");
-        long idx1 = store.add("test2");
-        long idx2 = store.add("test22");
-        long idx3 = store.add("test222");
-        long idx4 = store.add("test2222");
-        long idx5 = store.add("test22222");
         assertEquals(new String(store.get(idx0)), "test");
+        long idx1 = store.add("test2");
         assertEquals(new String(store.get(idx1)), "test2");
+        long idx2 = store.add("test22");
         assertEquals(new String(store.get(idx2)), "test22");
+        long idx3 = store.add("test222");
         assertEquals(new String(store.get(idx3)), "test222");
+        long idx4 = store.add("test2222");
         assertEquals(new String(store.get(idx4)), "test2222");
+        long idx5 = store.add("test22222");
         assertEquals(new String(store.get(idx5)), "test22222");
     }
 
@@ -28,6 +32,7 @@ class KeyStoreTest {
         //Insert value that will overflow initial size immediately
         KeyStore store = new KeyStore();
         String longString = "gbmgvyroigvourovsgbjsemtraojcstceznwgytcfgemkteuyvcpfkdfyxfblgehneoxxssttwbjgykyaelyknxjmousjlmqtmghpg";
+
         long idx0 = store.add(longString);
         assertEquals(new String(store.get(idx0)), longString);
     }
